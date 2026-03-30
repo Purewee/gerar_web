14a599b893c72eab723ead32a2f8176b8de491f2
Ulsbold <ulsbold.b0227@gmail.com>
2026-03-30 23:34:28 +0800

    domain verify meta

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+// This file customizes the <head> tag for the home page in Next.js App Router
+// Place your meta tags here to ensure they are rendered in the <head> section
+export default function Head() {
+  return (
+    <>
+      <meta name="facebook-domain-verification" content="bfj9qvfnuumo3b96iol46hzluo7s59" />
+    </>
+  );
+}
